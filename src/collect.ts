5a86d5884be0515e782a4386ec4e64d151c314a8
Dilip Kola <dilip@tensult.com>
2018-03-13 21:32:03 +0530

    Add single run script

--- src/collect.ts
+++ src/collect.ts
@@ -1,68 +1,43 @@
-import * as Collectors from './collectors';
 import { Dictionary } from './types';
 import { CollectorUtil } from './utils';
-import * as AWS from 'aws-sdk';
-import * as Cli from 'cli';
-import { writeFileSync } from 'fs';
+import * as Collectors from './collectors';
+import * as flat from 'flat';
 
-const result: Dictionary<any> = {};
-
-function copyDataToResult(keyPath, data) {
-    result[keyPath] = result[keyPath] || {};
-    for (let key in data) {
-        result[keyPath][key] = data[key];
-    }
-}
-
-let skippedCollectors = ['SnapshotsCollector'];
-function collect(collectors: any, keyPath?: string) {
-    for (let collectorName in collectors) {
-        if (keyPath && collectorName.endsWith('Collector') &&
-            collectors[collectorName] instanceof Function &&
-            skippedCollectors.indexOf(collectorName) === -1) {
-            // console.log("Executing", keyPath, collectorName)
-            CollectorUtil.cachedCollect(new collectors[collectorName]())
-                .then((data) => {
-                    copyDataToResult(keyPath, data);
-                }).catch((err) => {
-                    console.error(keyPath, collectorName, err);
-                });
-        } else if (collectors[collectorName]) {
-            const newKeyPath = keyPath ? `${keyPath}.${collectorName}` : collectorName
-            collect(collectors[collectorName], newKeyPath);
+export async function collect(moduleName?: string) {
+    const promises: Promise<any>[] = [];
+    const flatListOfCollectors = flat(Collectors);
+    
+    for (let collectorName in flatListOfCollectors) {
+        if(!collectorName.endsWith('Collector')) {
+            continue;
         }
+        if(moduleName && !collectorName.includes(moduleName)) {
+            continue;
+        }
+        const collectorPromise = CollectorUtil.cachedCollect(new flatListOfCollectors[collectorName]()).then((data) => {
+            const collectNameSpace = collectorName.replace(/.[A-Za-z]+$/, '');
+            return {
+                data,
+                namespace: collectNameSpace
+            }
+        }).catch((err) => {
+            console.error(collectorName, "failed", err);
+        });
+        promises.push(collectorPromise);
+    }
+    try{
+        const collectorResults = await Promise.all(promises);
+        return collectorResults.reduce((result, collectResult) => {
+            if(collectResult) {
+                result[collectResult.namespace] = result[collectResult.namespace] || {};
+                result[collectResult.namespace] = Object.assign(result[collectResult.namespace], collectResult.data);
+            }
+            return result;
+        }, {});
+    } catch(err) {
+        throw err;
     }
 }
-
-const cliArgs = Cli.parse({
-    profile: ['p', 'AWS profile name', 'string'],
-    output: ['o', 'output file name', 'file', 'collection_report.json'],
-    module: ['m', 'name of the module', 'string', 'all']
-});
-
-if (!cliArgs.profile) {
-    Cli.getUsage();
-}
-
-AWS.config.signatureVersion = 'v4';
-AWS.config.credentials = new AWS.SharedIniFileCredentials({
-    profile: cliArgs.profile
-});
-
-if (cliArgs.module != 'all') {
-    const moduleParts: string[] = cliArgs.module.split(".");
-    const filterdCollectors = moduleParts.reduce((collectors, modulePart) => {
-        return collectors[modulePart] || {};
-    }, Collectors)
-    collect(filterdCollectors, cliArgs.module);
-}
-else {
-    collect(Collectors);
-}
-
-process.on('exit', function () {
-    writeFileSync(cliArgs.output, JSON.stringify(result, null, 2), { encoding: 'utf-8' });
-});
 
 
 
--- src/collectors/aws/ebs/index.ts
+++ src/collectors/aws/ebs/index.ts
@@ -1,2 +1,1 @@
 export * from './volumes';
-export * from './snapshots';
