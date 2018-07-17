dc56d0081a8119d6c8a19ade72c7444e87047fa9
Dilip Kola <dilip@tensult.com>
2018-07-17 10:40:38 +0530

    Add Waiting in collect

--- src/collect.ts
+++ src/collect.ts
@@ -1,16 +1,16 @@
 import { Dictionary } from './types';
-import { CollectorUtil } from './utils';
+import { CollectorUtil, CommonUtil } from './utils';
 import * as Collectors from './collectors';
 import * as flat from 'flat';
 
 function getModules(moduleNames?: string | Array<string>) {
-    if(!moduleNames) {
+    if (!moduleNames) {
         return [];
     }
-    if(Array.isArray(moduleNames)) {
+    if (Array.isArray(moduleNames)) {
         return moduleNames;
     }
-    else if(typeof moduleNames === 'string' ){
+    else if (typeof moduleNames === 'string') {
         return moduleNames.split(',');
     }
     return [];
@@ -19,13 +19,13 @@ function getModules(moduleNames?: string | Array<string>) {
 export async function collect(moduleNames?: string | Array<string>) {
     const promises: Promise<any>[] = [];
     const flatListOfCollectors = flat(Collectors);
-    
+
     const modules = getModules(moduleNames);
     const filteredCollectorNames = Object.keys(flatListOfCollectors).filter((collectorName) => {
-        if(!collectorName.endsWith('Collector')) {
+        if (!collectorName.endsWith('Collector')) {
             return false;
         }
-        if(modules.length) {
+        if (modules.length) {
             return modules.some((moduleName) => {
                 return collectorName.split(".").indexOf(moduleName) !== -1;
             });
@@ -33,27 +33,30 @@ export async function collect(moduleNames?: string | Array<string>) {
         return true;
     });
     for (let collectorName of filteredCollectorNames) {
-        const collectorPromise = CollectorUtil.cachedCollect(new flatListOfCollectors[collectorName]()).then((data) => {
-            const collectNameSpace = collectorName.replace(/.[A-Za-z0-9]+$/, '');
-            return {
-                data,
-                namespace: collectNameSpace
-            }
-        }).catch((err) => {
-            console.error(collectorName, "failed", err);
-        });
+        console.info("Running", collectorName);
+        const collectorPromise = CollectorUtil.cachedCollect(new flatListOfCollectors[collectorName]())
+            .then((data) => {
+                const collectNameSpace = collectorName.replace(/.[A-Za-z0-9]+$/, '');
+                return {
+                    data,
+                    namespace: collectNameSpace
+                }
+            }).catch((err) => {
+                console.error(collectorName, "failed", err);
+            });
+        await CommonUtil.wait(500);
         promises.push(collectorPromise);
     }
-    try{
+    try {
         const collectorResults = await Promise.all(promises);
         return collectorResults.reduce((result, collectResult) => {
-            if(collectResult) {
+            if (collectResult) {
                 result[collectResult.namespace] = result[collectResult.namespace] || {};
                 result[collectResult.namespace] = Object.assign(result[collectResult.namespace], collectResult.data);
             }
             return result;
         }, {});
-    } catch(err) {
+    } catch (err) {
         throw err;
     }
 }
