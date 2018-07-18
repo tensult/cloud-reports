4dcc80edb9aeb58d3b2dddc693c17702605070be
Dilip Kola <dilip@tensult.com>
2018-07-18 09:58:52 +0530

    Add elastic ips usage Analyzer

--- src/analyzers/aws/ec2/elastic_ips_unused.ts
+++ src/analyzers/aws/ec2/elastic_ips_unused.ts
@@ -2,7 +2,7 @@ import { BaseAnalyzer } from '../../base'
 import { CheckAnalysisResult, ResourceAnalysisResult, Dictionary, SeverityStatus, CheckAnalysisType } from '../../../types';
 import { ResourceUtil } from '../../../utils';
 
-export class ElasticIPsUnusedAnalyzer extends BaseAnalyzer {
+export class ElasticIPsUsageAnalyzer extends BaseAnalyzer {
 
     analyze(params: any, fullReport?: any): any {
         const allElasticIPs = params.elastic_ips;
