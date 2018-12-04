37ad2951c09edc78c3c9d1107fac960a413a9904
Dilip Kola <dilip@tensult.com>
2018-12-04 15:26:41 +0530

    Add stack trace

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -6,23 +6,27 @@ export class LogUtil {
     }
 
     public static log(...params) {
+        this.printStack(params[0]);
         console.log(...params);
     }
 
     public static info(...params) {
         if (LogUtil.LogLevel.Info >= LogUtil.currentLogLevel) {
+            this.printStack(params[0]);
             console.info("[INFO]", ...params);
         }
     }
 
     public static warn(...params) {
         if (LogUtil.LogLevel.Warn >= LogUtil.currentLogLevel) {
+            this.printStack(params[0]);
             console.warn("[WARN]", ...params);
         }
     }
 
     public static error(...params) {
         if (LogUtil.LogLevel.Error >= LogUtil.currentLogLevel) {
+            this.printStack(params[0]);
             console.error("[ERROR]", ...params);
         }
     }
@@ -36,4 +40,15 @@ export class LogUtil {
     };
 
     private static currentLogLevel = LogUtil.LogLevel.Warn;
+
+    private static printStack(params: any[]) {
+        if (!params || !params.length) {
+            return;
+        }
+        for (const param of params) {
+            if (param && param instanceof Error) {
+                console.log("StackTrace:\n", param.stack);
+            }
+        }
+    }
 }
