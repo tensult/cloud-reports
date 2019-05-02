9d123845b5925f00904dea79d0d19bdec9d1a281
Kunal Raj Bhardwaj <kunalraj1705@gmaiil.com>
2019-05-02 16:24:26 +0530

    Modified file to fetch redshift data

--- src/collectors/aws/redshift/index.ts
+++ src/collectors/aws/redshift/index.ts
@@ -1,2 +1,7 @@
 export * from "./audit_logs";
 export * from "./clusters";
+export * from "./event";
+export * from  "./event_subscription";
+export * from "./node_reservation";
+export * from  "./subnet_groups";
+export * from  "./snapshot"
