301a327fb538909b7263acaa403cfb6dc4ba8f84
Harshul1310 <harshulbansal1310@gmail.com>
2019-05-08 15:38:23 +0530

    Index Updated

--- src/collectors/aws/cloudfront/index.ts
+++ src/collectors/aws/cloudfront/index.ts
@@ -1,2 +1,4 @@
 export * from "./distribution_configs";
 export * from "./distributions";
+export * from "./cloud_front_origin_access_identity";
+export * from "./cloud_front_origin_access_identity_configs";
