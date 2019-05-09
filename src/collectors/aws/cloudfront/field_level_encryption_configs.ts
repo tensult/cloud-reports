89f6b31e43a13007b80bbbeeefcf552816d45edd
Harshul1310 <harshulbansal1310@gmail.com>
2019-05-09 14:58:29 +0530

    services for cloudfront

---
+++ src/collectors/aws/cloudfront/field_level_encryption_configs.ts
@@ -0,0 +1,31 @@
+import * as AWS from "aws-sdk";
+import { CollectorUtil, CommonUtil } from "../../../utils";
+import { AWSErrorHandler } from "../../../utils/aws";
+import { BaseCollector } from "../../base";
+import {  FieldLevelEncryptionCollector } from "./field_level_encryption";
+
+export class  FieldLevelEncryptionconfigsCollector extends BaseCollector {
+    public collect() {
+        return this.listAllFieldLevelEncryptionconfigs();
+    }
+
+    private async listAllFieldLevelEncryptionconfigs() {
+        try {
+            const cloudfront = this.getClient("CloudFront", "us-east-1") as AWS.CloudFront;
+            const field_level_encrypt_Collector = new FieldLevelEncryptionCollector();
+            field_level_encrypt_Collector.setSession(this.getSession());
+            const field_level_encrypt_Data = await CollectorUtil.cachedCollect(field_level_encrypt_Collector);
+            const field_level_encrypt_configs = {};
+            for (const field_level_encrypt of field_level_encrypt_Data.field_level_encrypt) {
+                const cloudfrontFieldLevelEncryptionData:
+                    AWS.CloudFront.GetFieldLevelEncryptionConfigResult =
+                    await cloudfront.getFieldLevelEncryptionConfig({ Id: field_level_encrypt.Id }).promise();
+                    field_level_encrypt_configs[field_level_encrypt.Id] = cloudfrontFieldLevelEncryptionData.FieldLevelEncryptionConfig;
+                await CommonUtil.wait(200);
+            }
+            return { field_level_encrypt_configs };
+        } catch (error) {
+            AWSErrorHandler.handle(error);
+        }
+    }
+}
--- src/collectors/aws/cloudfront/index.ts
+++ src/collectors/aws/cloudfront/index.ts
@@ -4,3 +4,5 @@ export * from "./cloud_front_origin_access_identity";
 export * from "./cloud_front_origin_access_identity_configs";
 export * from "./streaming_distributions_configs";
 export * from "./streaming_distributions";
+export * from "./field_level_encryption_configs";
+export * from "./field_level_encryption";
