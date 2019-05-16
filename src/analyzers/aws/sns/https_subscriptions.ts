516374be6ca59b8604137a40f62bada9baeb2615
Harshul1310 <harshulbansal1310@gmail.com>
2019-05-16 16:14:38 +0530

    changes made

--- src/analyzers/aws/sns/https_subscriptions.ts
+++ src/analyzers/aws/sns/https_subscriptions.ts
@@ -4,13 +4,13 @@ import {
 } from "../../../types";
 import { BaseAnalyzer } from "../../base";
 
-export class TopicsWithHttpsSubscriptionsAnalyzer extends BaseAnalyzer {
-    public  checks_what : string = "Are there any SNS topics without HTTPS subscriptions  ?";
-    public  checks_why : string = `Ensure that none of the Amazon SNS subscriptions created within your AWS account are using
-    HTTP instead of HTTPS as delivery protocol in order to enforce SSL encryption for all subscription requests.`;
-    public checks_recommendation: string = `Every SNS topic should have
-    proper HTTPS subscriptions else you should remove it`
-    public checks_name : string = "Topic";
+export class ProtocolsWithHttpSubscriptionsAnalyzer extends BaseAnalyzer {
+    public  checks_what : string = "Are there any SNS topics without HTTP subscriptions but  ?";
+    public  checks_why : string = `Topics without subscriptions cause confusion as
+    mistakenly we might be publishing to them but no one will receive them`;
+    public checks_recommendation : string = `Every SNS topic should have
+    proper subscriptions else you should remove it`;
+    public checks_name : string = "Protocol";
     public analyze(params: any, fullReport?: any): any {
         const allSubscriptions = params.subscriptions;
         const allTopics = params.topics;
@@ -18,14 +18,14 @@ export class TopicsWithHttpsSubscriptionsAnalyzer extends BaseAnalyzer {
         if (!allTopics || !allSubscriptions) {
             return undefined;
         }
-        const topics_with_https_subscriptions: ICheckAnalysisResult = { type: CheckAnalysisType.OperationalExcellence };
-        topics_with_https_subscriptions.what = this.checks_what;
-        topics_with_https_subscriptions.why = this.checks_why;
-        topics_with_https_subscriptions.recommendation = this.checks_recommendation;
+        const protocols_without_https: ICheckAnalysisResult = { type: CheckAnalysisType.OperationalExcellence };
+        protocols_without_https.what = this.checks_what;
+        protocols_without_https.why = this.checks_why;
+        protocols_without_https.recommendation = this.checks_recommendation;
         const allRegionsAnalysis: IDictionary<IResourceAnalysisResult[]> = {};
         for (const region in allTopics) {
             const regionTopics = allTopics[region];
-            const regionSubscriptionsMap = this.mapHTTPSSubscriptionByTopicArn(allSubscriptions[region]);
+            const regionSubscriptionsMap = this.mapSubscriptionByTopicArn(allSubscriptions[region]);
 
             allRegionsAnalysis[region] = [];
             for (const topic of regionTopics) {
@@ -37,22 +37,22 @@ export class TopicsWithHttpsSubscriptionsAnalyzer extends BaseAnalyzer {
                 };
                 if (regionSubscriptionsMap[topic.TopicArn] && regionSubscriptionsMap[topic.TopicArn].length) {
                     topic_analysis.severity = SeverityStatus.Good;
-                    topic_analysis.message = "Topic has HTTPS subscriptions";
+                    topic_analysis.message = "Topic has subscriptions";
                 } else {
                     topic_analysis.severity = SeverityStatus.Warning;
-                    topic_analysis.message = "Topic does not have HTTPS subscriptions";
-                    topic_analysis.action = "Either add HTTPS subscription or delete the topic";
+                    topic_analysis.message = "Topic does not have any subscriptions";
+                    topic_analysis.action = "Either add subscription or delete the topic";
                 }
 
                 allRegionsAnalysis[region].push(topic_analysis);
             }
         }
-        topics_with_https_subscriptions.regions = allRegionsAnalysis;
-        return { topics_with_https_subscriptions };
+        protocols_without_https.regions = allRegionsAnalysis;
+        return { protocols_without_https };
     }
 
-    private mapHTTPSSubscriptionByTopicArn(https_subscriptions: any[]): IDictionary<any[]> {
-        return https_subscriptions.reduce((subscriptionsMap, subscription) => {
+    private mapSubscriptionByTopicArn(subscriptions: any[]): IDictionary<any[]> {
+        return subscriptions.reduce((subscriptionsMap, subscription) => {
             subscriptionsMap[subscription.TopicArn] = subscriptionsMap[subscription.TopicArn] || [];
             subscriptionsMap[subscription.TopicArn].push(subscription);
             return subscriptionsMap;
