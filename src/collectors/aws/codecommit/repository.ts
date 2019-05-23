64a3e6c161e6b2c637f3b5aa1ff04220041cc30f
Reebu Abraham <inamarina9627@gmail.com>
2019-05-23 16:54:33 +0530

    Collector-CodeCommit

--- src/collectors/aws/codecommit/repository.ts
+++ src/collectors/aws/codecommit/repository.ts
@@ -2,7 +2,6 @@ import * as AWS from "aws-sdk";
 import { CommonUtil } from "../../../utils";
 import { AWSErrorHandler } from "../../../utils/aws";
 import { BaseCollector } from "../../base";
-// import {CodeCommitRepositoryListCollector} from "./repository_list";
 
 export class CodeCommitRepositoryCollector extends BaseCollector {
     public collect(callback: (err?: Error, data?: any) => void) {
