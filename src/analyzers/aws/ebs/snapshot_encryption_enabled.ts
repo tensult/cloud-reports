665b664caeb31a3999be8722786b2f6ea8707cd4
Harshul1310 <harshulbansal1310@gmail.com>
2019-05-28 15:09:11 +0530

    ebs best case for snapshot data at rest encryption

--- src/analyzers/aws/ebs/snapshot_encryption_enabled.ts
+++ src/analyzers/aws/ebs/snapshot_encryption_enabled.ts
@@ -10,45 +10,36 @@ export class SnapshotEncryptionEnabledAnalyzer extends BaseAnalyzer {
     public analyze(params: any, fullReport?: any): any {
         const allVolumes = params.volumes;
         const allSnapshots = params.snapshots;
-        if (!allVolumes||!allSnapshots) {
+        if (!allVolumes || !allSnapshots) {
             return undefined;
         }
-       const snapshots_encryption_enabled: ICheckAnalysisResult = { type: CheckAnalysisType.Security };
-       snapshots_encryption_enabled.what = "Are EBS volumes encrypted at rest?";
-       snapshots_encryption_enabled.why = "Data at rest should always be encrypted";
-       snapshots_encryption_enabled.recommendation = "Recommended to enable encryption for EBS volumes";
+        const snapshots_encryption_enabled: ICheckAnalysisResult = { type: CheckAnalysisType.Security };
+        snapshots_encryption_enabled.what = "Are EBS snapshots are encryptedt?";
+        snapshots_encryption_enabled.why = "Snapshots should always be encrypted when data is at rest.";
+        snapshots_encryption_enabled.recommendation = "Recommended to enable encryption for EBS snapshots.";
         const allRegionsAnalysis: IDictionary<IResourceAnalysisResult[]> = {};
         for (const region in allVolumes) {
             const regionVolumes = allVolumes[region];
             allRegionsAnalysis[region] = [];
             for (const snapshots of regionVolumes) {
                 const snapshotsAnalysis: IResourceAnalysisResult = {};
-                snapshotsAnalysis.resource = {snapshots: allSnapshots[region]};
-                //console.log("snapshots",snapshots)
+                snapshotsAnalysis.resource = snapshots;
                 snapshotsAnalysis.resourceSummary = {
-                    name: "Volume",
-                    value: `${snapshots.Progress} | ${snapshots.Encrypted}`,
+                    name: "Snapshots",
+                    value: `${ResourceUtil.getNameByTags(snapshots)} | ${snapshots.Encrypted}`,
                 };
-                const Encrpyt = snapshots.Encrypted;
-                console.log("encryption",Encrpyt);
-                if (Encrpyt.indexOf("false") != -1) {
+                if (snapshots.Encrypted) {
                     snapshotsAnalysis.severity = SeverityStatus.Good;
-                    snapshotsAnalysis.message = "Encryption enabled";
-                    console.log("encryption",Encrpyt);
+                    snapshotsAnalysis.message = "Encryption enabled.";
                 } else {
                     snapshotsAnalysis.severity = SeverityStatus.Warning;
-                    snapshotsAnalysis.message = "Encryption not enabled";
-                    snapshotsAnalysis.action = "Enable encryption at rest";
+                    snapshotsAnalysis.message = "Encryption not enabled.";
+                    snapshotsAnalysis.action = "Enable encryption at rest.";
                 }
                 allRegionsAnalysis[region].push(snapshotsAnalysis);
             }
         }
-       snapshots_encryption_enabled.regions = allRegionsAnalysis;
-        return {snapshots_encryption_enabled };
+        snapshots_encryption_enabled.regions = allRegionsAnalysis;
+        return { snapshots_encryption_enabled };
     }
-    // private getEncryptionData(snapshots: any[]){
-
-    //     const encrypt = snapshots.Encrypted;
-    //     return encrypt;
-    // } 
 }
